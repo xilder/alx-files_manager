5ffc27ae2ab093aab0e48a75ade1fbdc1efe3b87
xilder <olabowaleayobami@gmail.com>
2024-08-04 11:56:58 +0100

    adds task 0

---
+++ utils/redis.js
@@ -0,0 +1,33 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.isConnected = true;
+    this.client.on('error', (error) => {
+      console.log(error);
+      this.isConnected = false;
+    });
+  }
+  isAlive() {
+    return this.isConnected;
+  }
+  async get(key) {
+    const redisGet = promisify(this.client.get).bind(this.client);
+    const value = await redisGet(key);
+    return value;
+  }
+  async set(key, value, exp) {
+    const redisSetex = promisify(this.client.setex).bind(this.client);
+    await redisSetex(key, exp, value);
+  }
+  async del(key) {
+    const redisDel = promisify(this.client.del).bind(this.client);
+    await redisDel(key);
+  }
+}
+
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
