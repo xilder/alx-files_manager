3a057104a4d184dd44e2a69d72642a2d90b5e217
xilder <olabowaleayobami@gmail.com>
2024-08-04 17:46:24 +0100

    adds task 0 and 1

---
+++ utils/db.js
@@ -0,0 +1,25 @@
+import MongoClient from 'mongodb/lib/mongo_client';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const db = process.env.DB_DATABASE || 'files_manager';
+    const URL = `mongodb://${host}:${port}/${db}`;
+    this.client = new MongoClient(URL, { useUnifiedTopology: true });
+    this.client.connect();
+  }
+  isAlive() {
+    return this.client.isConnected();
+  }
+  async nbUsers() {
+    return this.client.db().collection('users').countDocuments();
+  }
+  async nbFiles() {
+    return this.client.db().collection('files').countDocuments();
+  }
+}
+
+const dbclient = new DBClient();
+
+module.exports = dbclient
